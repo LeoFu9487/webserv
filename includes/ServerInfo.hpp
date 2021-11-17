95d31ccc2945726dfe1b188ec807c996b8f5276c
Pepe <xinyun.pepe@gmail.com>
2021-11-17 13:04:13 +0100

    serverinfo

--- includes/ServerInfo.hpp
+++ includes/ServerInfo.hpp
@@ -6,37 +6,44 @@
 /*   By: xli <xli@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:01:26 by xli               #+#    #+#             */
-/*   Updated: 2021/11/17 09:22:49 by xli              ###   ########lyon.fr   */
+/*   Updated: 2021/11/17 11:15:38 by xli              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "webserv.hpp"
 #include <iostream>
+#include <vector>
+#include <string>
 
 class ServerInfo
 {
 	private:
-		int			_port;
-		std::string	_IP;
-		std::string	_error_pages;
-		int			_max_client;
+		int							_port;
+		std::vector<std::string>	_server_names;
+		std::string					_host;
+		std::string					_error_pages;
+		int							_max_client;
 
 	public:
 		void	print() const;
 
 		ServerInfo();
 		ServerInfo(const ServerInfo &copy);
-		ServerInfo operator=(const ServerInfo &copy);
+		ServerInfo &operator=(const ServerInfo &copy);
 		~ServerInfo();
 
-		int					get_ort() const;
-		std::string const	&get_IP() const;
-		std::string const	&get_error_page() const;
-		int					getMaxClient() const;
-		void				set_port();
-		void				set_error_page();
-		void				set_max_client();
+		int const						get_port() const;
+		std::vector<std::string> const	&get_names() const;
+		std::string const				&get_host() const;
+		std::string const				&get_error() const;
+		int const						get_max_client() const;
+
+		void							set_port(const char *);
+		void							set_names(const char *);
+		void							set_host(const char *);
+		void							set_error(const char *);
+		void							set_max_client(const char *);
 };
 
 /*
--- srcs/conf/ServerInfo.cpp
+++ srcs/conf/ServerInfo.cpp
@@ -6,17 +6,21 @@
 /*   By: xli <xli@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 17:01:04 by xli               #+#    #+#             */
-/*   Updated: 2021/11/17 09:21:05 by xli              ###   ########lyon.fr   */
+/*   Updated: 2021/11/17 11:10:47 by xli              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ServerInfo.hpp"
 
 ServerInfo::ServerInfo() : _port(0), _max_client(0) {}
+
 ServerInfo::ServerInfo(const ServerInfo &copy)
 :	_port(copy._port),
-	_error_page(copy._error_page),
+	_server_names(copy._server_names),
+	_host(copy._host),
+	_error_pages(copy._error_pages),
 	_max_client(copy._max_client) {}
+
 ServerInfo &ServerInfo::operator=(const ServerInfo &copy)
 {
 	ServerInfo	tmp(copy);
@@ -25,26 +29,39 @@ ServerInfo &ServerInfo::operator=(const ServerInfo &copy)
 }
 ServerInfo::~ServerInfo() {}
 
-int	ServerInfo::get_port() const
+/*
+** =============== GETTER ===============
+*/
+
+int const ServerInfo::get_port() const { return _port; }
+
+std::vector<std::string> const &ServerInfo::get_names() const { return _server_names; }
+
+std::string const &ServerInfo::get_host() const { return _host; }
+
+std::string const &ServerInfo::get_error() const { return _error_pages; }
+
+int const ServerInfo::get_max_client() const { return _max_client; }
+
+
+/*
+** =============== SETTER ===============
+*/
+
+void ServerInfo::set_port(const char *p)
 {
-	return _port;
+	
+	_port = atoi(p);
 }
 
-std::string const	&ServerInfo::get_error_page() const
-{
-	return _error_page;
-}
 
-int	ServerInfo::getMaxClient() const
-{
-	return _max_client;
-}
+
 
 #ifdef TEST
 ServerInfo::ServerInfo()
 {
 	_port = 8080;
-	_IP = "0.0.0.0";
-	_maxClient = 100;
+	_host = "0.0.0.0";
+	_max_client = 100;
 }
 #endif
