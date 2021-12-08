ea84053928f4abc93fecaf73a3e94af17e74dad4
Yu-peng Fu <yfu@z4r6p5.42lyon.fr>
2021-12-08 16:08:10 +0100

    html ok

--- www/joke_files/joke_cgis/get_joke.sh
+++ www/joke_files/joke_cgis/get_joke.sh
@@ -2,21 +2,10 @@ DIR_NAME="www/joke_files/jokes"
 STR="$QUERY_STRING"
 printf "Content-Type: text/plain\r\n\n"
 
-if [ -z "$STR" ]
-then
-	if [ -z "$(ls $DIR_NAME)" ]
-	then
-		echo "There are no jokes. Upload some jokes first."
-		return 0
-	fi
-	JOKE="$(ls $DIR_NAME | shuf -n 1)"
-	FILE="$DIR_NAME/$JOKE"
-else
-	IFS="="
-	set $STR
-	JOKE="joke_$2"
-	FILE="$DIR_NAME/$JOKE"
-fi
+IFS="="
+set $STR
+JOKE="joke_$2"
+FILE="$DIR_NAME/$JOKE"
 
 echo "$JOKE :\n\n"
 
--- www/joke_files/joke_cgis/post_joke.py
+++ www/joke_files/joke_cgis/post_joke.py
@@ -1,5 +1,5 @@
 import urllib.parse, os
-string = input()[8:]
+string = urllib.parse.unquote_plus(input()[8:])
 for i in range(0, 100):
 	if os.path.isfile('www/joke_files/jokes/joke_' + str(i)) == False:
 		f = open('www/joke_files/jokes/joke_' + str(i), 'x')
@@ -8,12 +8,12 @@ for i in range(0, 100):
 		print('Status: 201\r')
 		print('Content-Type: text/html\r\n')
 		print('The joke is saved as joke_' + str(i) + '\r')
-		print('<li><a href="/joke">back to the joke page</a></li>')
+		print('<li><a href="/test_joke.html">back to joke page</a></li>')
 		break
 	elif i == 99:
 		print('Status: 422\r')
 		print('Content-Type: text/html\r\n')
 		print('Status : 422 Unprocessable Entity.\r')
 		print('There are already too many jokes!\r')
-		print('<li><a href="/joke">back to the joke page</a></li>')
+		print('<li><a href="/test_joke.html">back to joke page</a></li>')
 		break
